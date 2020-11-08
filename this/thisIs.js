d506b7ada04a22962432ca20eabe1d30ed18edd5
chonnavypong <chonnavypong@gmail.com>
2020-11-08 20:34:22 +0700

    x

--- this/thisIs.js
+++ this/thisIs.js
@@ -1,19 +1,33 @@
 'use strict'
 
-function asim() {
-    console.log(this)
-}
+// function asim() {
+//     console.log(this)
+// }
 
-asim()
-asim.call(window)
+// asim()
+// asim.call(window)
 
 // const asim = {
 //     checkThis: function() {
 //         function checkOther() {
-//          console.log('This is: ',this)   
+//          console.log(this)   
 //         }
 //     checkOther.call(this)
 //     }
 // }
 
 // asim.checkThis()
+
+var animal = {
+    kind: 'cow',
+    which: function(){
+        console.log(this)
+        console.log(this.kind)
+    }
+}
+
+// this = undifined ดังนั้น undifined.kind จึง throw error ออกมา
+var animalFunc = animal.which
+// animalFunc()
+animalFunc.call(animal)
+// animal.which()
